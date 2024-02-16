52b75462e426e9d92c5565aca6f4d12e78b8e75a
Andy <andy.liu@databricks.com>
2024-02-16 02:16:11 -0800

    feat: add test data_frame

---
+++ test.py
@@ -0,0 +1,41 @@
+import sys
+import collections.abc
+from dbruntime import UserNamespaceInitializer
+
+def main():
+    # Parse arguments from sys.argv into dictionary
+    # Repeated arguments will be grouped into an array
+    """
+        --securable
+            Securable that needs to be materialized
+        --predicates
+            Predicates JSON to push down
+        --hadoop
+            Hadoop configuration to set
+        --location
+            The location the materialization will be stored
+    """
+    args = {}
+    for arg in sys.argv[1:]:
+        split = arg.split("=", 1)
+        key = split[0]
+        if key.startswith("--"):
+            key = key[2:]
+        val = split[1]
+        if key in args:
+            if not isinstance(args[key], collections.abc.Sequence):
+                args[key] = [args[key]]
+            args[key].append(val)
+        else:
+            args[key] = val
+
+    user_namespace_initializer = UserNamespaceInitializer.getOrCreate()
+    spark = user_namespace_initializer.namespace_globals["spark"]
+
+    # Read the securable
+    securable = args["securable"]
+    data_frame = spark.read.table(securable)
+    data_frame.show()
+    
+if __name__ == '__main__':
+    main()
