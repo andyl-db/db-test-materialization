057b31f98cf99bdf5a13fd377f8de1ec77886f83
Andy <andy.liu@databricks.com>
2024-04-11 00:05:10 -0700

    test 3

--- task.py
+++ task.py
@@ -1,5 +1,32 @@
-import parse_args
+import sys
+import collections.abc
 from dbruntime import UserNamespaceInitializer
+
+def parse_args():
+  """
+  Parse arguments from sys.argv into dictionary.
+  Repeated arguments will be grouped into an array.
+
+  For example, "--foo=bar --foo=party --bar=foo" will generate the following:
+    dict(
+      "foo": ["bar", "party"],
+      "bar": "foo"
+    )
+  """
+  args = {}
+  for arg in sys.argv[1:]:
+      split = arg.split("=", 1)
+      key = split[0]
+      if key.startswith("--"):
+          key = key[2:]
+      val = split[1]
+      if key in args:
+          if not isinstance(args[key], collections.abc.Sequence):
+              args[key] = [args[key]]
+          args[key].append(val)
+      else:
+          args[key] = val
+  return args
 
 def main():
     """
